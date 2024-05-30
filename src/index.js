6e8c4afbfa38d10a1beb6833df2537613ef412ea
ramireddynusum <ramireddy.nusum@zemosolabs.com>
2024-05-30 12:45:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-// "use strict";
-// var __importDefault = (this && this.__importDefault) || function (mod) {
-//     return (mod && mod.__esModule) ? mod : { "default": mod };
-// };
-// Object.defineProperty(exports, "__esModule", { value: true });
-// const express_1 = __importDefault(require("express"));
-// const app = (0, express_1.default)();
-// const port = process.env.PORT || 3000;
-// app.get('/', (req, res) => {
-//     res.send('this is homepage');
-// });
-// app.listen(port, () => {
-//     console.log('server is running at port number 3000');
-// });
+
