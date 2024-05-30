6a96330fe9fb7462603c627963ef08e7a517f6d3
ramireddynusum <ramireddy.nusum@zemosolabs.com>
2024-05-30 14:57:16 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-const ap_p = express();
+const app = express();
 const port = process.env.PORT || 3000;
-ap_p.get('/',(req,res)=>{
+app.get('/',(req,res)=>{
     res.send('this is homepage')
-})
-ap_p.listen( port ,()=>{
+
+app.listen( port ,()=>{
     console.log('server is running at port number 3000')
 });
